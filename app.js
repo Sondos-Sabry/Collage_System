b26e0ba634d6c7b5bbf1215c0eec49a39c98f283
Sondos Hammad <89040514+Sondos-Sabry@users.noreply.github.com>
2023-05-25 01:23:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -59,8 +59,77 @@ app.get("/", (req, res) => {
   res.render("Home");
 });
 
-app.get("/login", (req, res) => {
-  res.render("Login");
+//form to to login for student , admin and doctor
+app.get("/login", function (req, res) {
+  res.render("Login", { error: req.query.error });
+});
+// Handle the form submission
+app.post("/login", (req, res) => {
+  console.log(req.body);
+
+  const Email = req.body.Email;
+  const Id = req.body.Id;
+  const user = req.body.user;
+
+  if (Email && Id) {
+    if (user === "Student") {
+      const query = "SELECT * FROM student WHERE Email = ? AND Id = ?";
+      connection.query(query, [Email, Id], (error, data) => {
+        if (data.length > 0) {
+          if ((data.Id = Id)) {
+            req.Id = data.Id;
+            res.redirect("student_home");
+          } else if (data.Id != Id) {
+            res.render("login", { error: "Incorrect Id" });
+          }
+        } else {
+          res.render("login", {
+            error: "please make sure from your email and id ",
+          });
+        }
+        res.end();
+      });
+    } else if (user === "Doctor") {
+      const query = "SELECT * FROM doctor WHERE Email = ? AND Id = ?";
+      connection.query(query, [Email, Id], (error, data) => {
+        if (data.length > 0) {
+          if ((data.Id = Id)) {
+            req.Id = data.Id;
+            res.redirect("doctor_home");
+          } else {
+            res.render("login", { error: "Incorrect Id" });
+          }
+        } else {
+          res.render("login", { error: "please sure from your email and id " });
+        }
+        res.end();
+      });
+    } else if (user === "Admin") {
+      const query = "SELECT * FROM admin WHERE Email = ? AND Id = ?";
+      connection.query(query, [Email, Id], (error, data) => {
+        if (data.length > 0) {
+          if ((data.Id = Id)) {
+            req.Id = data.Id;
+            res.redirect("admin_home");
+          } else {
+            res.render("login", { error: "Incorrect Id" });
+          }
+        } else {
+          res.render("login", {
+            error: "please make sure from your email and id ",
+          });
+        }
+        res.end();
+      });
+    } else {
+      res.render("login", {
+        error: "Please Enter Email Address and Your Academic Id",
+      });
+
+      res.end();
+    }
+  }
+
 });
 app.get("/student_home", (req, res) => {
   res.render("student_home");
